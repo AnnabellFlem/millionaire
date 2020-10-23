38f778f5e4b2f67090f1378bb2a61fffc527d65c
Anna Tsukanova <anna.tsukanova@kyivstar.net>
2020-10-23 21:31:43 +0300

    fixes

--- src/__mocks__/mockJokeLIst.js
+++ src/__mocks__/mockJokeLIst.js
@@ -1,24 +1,30 @@
-export const MOCK__QUESTIONS = [{
-  id: 1,
-  question: 'a',
-  answers: 'aa'
-},{
-  id: 2,
-  question: 'a',
-  answers: 'aa'
-},{
-  id: 3,
-  question: 'a',
-  answers: [{
+export const MOCK__QUESTIONS = [
+  {
     id: 1,
-    answer: 'aaa',
-    right: true
+    question: 'a',
+    answers: 'aa',
   },
-    {
-      id: 2,
-      answer: 'bb',
-      right: false
-    }]
-}]
+  {
+    id: 2,
+    question: 'a',
+    answers: 'aa',
+  },
+  {
+    id: 3,
+    question: 'a',
+    answers: [
+      {
+        id: 1,
+        answer: 'aaa',
+        right: true,
+      },
+      {
+        id: 2,
+        answer: 'bb',
+        right: false,
+      },
+    ],
+  },
+]
 
-export const MOCK__Money = ['1', '20', '100000']
+export const MOCK__MONEY = ['1', '20', '100000']
