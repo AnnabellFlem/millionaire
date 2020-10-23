aa2b105ede2d18bf1c41f30663cb3c4f67b27ef4
Anna Tsukanova <anna.tsukanova@kyivstar.net>
2020-10-23 17:10:27 +0300

    prettier fixes

--- src/Components/BaseComponents/Button/Button.tsx
+++ src/Components/BaseComponents/Button/Button.tsx
@@ -5,21 +5,27 @@ import './Button.css'
 type Props = React.DetailedHTMLProps<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
-  > & Partial<{
-  size: 's' | 'm'
-  fullWidth: boolean
-}>
+> &
+  Partial<{
+    size: 's' | 'm'
+    fullWidth: boolean
+  }>
 
-const Button: React.FC<Props> = ({ size = 'm', fullWidth, children, ...rest }) => {
+const Button: React.FC<Props> = ({
+  size = 'm',
+  fullWidth,
+  children,
+  ...rest
+}) => {
   const defaultButtonClassName = 'btn'
   const buttonClassNames = classNames(defaultButtonClassName, {
     [`${defaultButtonClassName}--${size}`]: size,
-    [`${defaultButtonClassName}--fullWidth`]: fullWidth
+    [`${defaultButtonClassName}--fullWidth`]: fullWidth,
   })
 
   return (
-    <button className={ buttonClassNames } { ...rest }>
-      { children }
+    <button className={buttonClassNames} {...rest}>
+      {children}
     </button>
   )
 }
