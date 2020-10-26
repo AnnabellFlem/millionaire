2f7b9b2683cb309590b68fcc985522dde37247ae
Anna Tsukanova <anna.tsukanova@kyivstar.net>
2020-10-26 10:17:19 +0200

    Updates

---
+++ precache-manifest.75dbf287db1447e03f7668870271934c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff6e0cb157fb3e12b9d1794eeb36703",
+    "url": "/millionaire/index.html"
+  },
+  {
+    "revision": "22c9f591daf22d0b05a6",
+    "url": "/millionaire/static/css/main.f4a00e33.chunk.css"
+  },
+  {
+    "revision": "6295df4906b65ea95a64",
+    "url": "/millionaire/static/js/2.f10450b4.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/millionaire/static/js/2.f10450b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c9f591daf22d0b05a6",
+    "url": "/millionaire/static/js/main.841c5b6e.chunk.js"
+  },
+  {
+    "revision": "e977f01e682fc9fa8fa3",
+    "url": "/millionaire/static/js/runtime-main.655771d3.js"
+  },
+  {
+    "revision": "8363766b028dfbcd399c9353e454d915",
+    "url": "/millionaire/static/media/close.8363766b.svg"
+  },
+  {
+    "revision": "8427ffe9450c4710e2fbc3c49566a382",
+    "url": "/millionaire/static/media/menu.8427ffe9.svg"
+  },
+  {
+    "revision": "7164b91f6237a29dad946f52b845be1b",
+    "url": "/millionaire/static/media/thumbs-up.7164b91f.svg"
+  }
+]);
